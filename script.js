fb05cd3ace8e5991b9a3ad2059111bedfca3370a
estudante-start <sousa.murilog@gmail.com>
2025-10-21 16:35:44 -0300

    alert e prompt

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("olá mundo!")
+alert("Bem vindo ao meu portifólio!");
+let nome = prompt("Qual é o seu nome?");
+
+let texto = document.querySelector("#texto");
+ 
+texto.textContent = nomeUsuário;
