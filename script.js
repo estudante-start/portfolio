7eb991e6678487e937b70bb30027dfa1b3b6ac2a
estudante-start <sousa.murilog@gmail.com>
2025-11-10 14:42:03 -0300

    desafio de javascript

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ let texto = document.querySelector("#nome")
 
 let nome = prompt("Qual é o seu nome?")
 
-texto.textContent = nome;
+if(nome == null){
+    texto.textContent = "Seja bem vindo!"
+}else{
+   texto.textContent = nome;
+}
+
 
