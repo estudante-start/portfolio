197d7aa69ad6d011ec84fdc1cfb9c67d688f19f5
estudante-start <sousa.murilog@gmail.com>
2025-11-11 11:06:00 -0300

    r9 final

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let texto = document.querySelector("#nome")
 // texto.textContent = "Monalize"
 
 let nome = prompt("Qual é o seu nome?")
+texto.textContent = nome;
 
-if(nome == null){
-    texto.textContent = "Seja bem vindo!"
-}else{
-   texto.textContent = nome;
-}
-
-
+// Desafio Extra
+// if(nome == null){
+//     texto.textContent = "Seja bem vindo!"
+// }else{
+//    texto.textContent = nome;
+// } 
